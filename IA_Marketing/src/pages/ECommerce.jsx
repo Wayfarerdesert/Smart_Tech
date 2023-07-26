b7cef8bc565df124c45a0a1c16e54aa54ba5a6c9
Wayfarerdesert <facundo355@outlook.com>
2023-07-26 10:23:29 +0200

    PdfDownloader OK

--- IA_Marketing/src/pages/ECommerce.jsx
+++ IA_Marketing/src/pages/ECommerce.jsx
@@ -20,7 +20,7 @@ import {
   CategoryScale,
   Tooltip,
   Title,
-  Legend
+  Legend,
 } from "chart.js";
 import Map from "./graphics/Geomap";
 
@@ -36,47 +36,90 @@ ChartJS.register(
 );
 // Fin de Elementos importados para el mapa
 
+import React, { useRef } from "react";
+import html2pdf from "html2pdf.js";
 
 const ECommerce = () => {
+  const contentRef = useRef(null);
+
+  const chartWidth = 80;
+  const chartHeight = 100;
+
+
+  const exportToPDF = () => {
+    console.log("Button clicked, exportToPDF function called!");
+    const content = contentRef.current;
+
+    // Calculate the number of charts that can fit horizontally and vertically
+    const horizontalFit = Math.floor((190 - 2 * 5) / chartWidth); // 5 mm spacing between charts
+    const verticalFit = Math.floor((277 - 2 * 5) / chartHeight); // 5 mm spacing between charts
+
+    // Calculate the final number of charts that can fit on each A4 page
+    const chartsPerPage = horizontalFit * verticalFit;
+
+    // Now you have the number of charts that can fit on each A4 page
+    console.log("Charts per A4 page:", chartsPerPage);
+
+    const opt = {
+      margin: 10,
+      filename: "ecommerce_charts.pdf",
+      image: { type: "jpeg", quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+    };
+
+    html2pdf().set(opt).from(content).save();
+  };
+
   return (
     <>
-      <div className="grid grid-cols-1 gap-3 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-        <CardOne />
-        <CardTwo />
-        <CardThree />
-        <CardFour />
-      </div>
+      <div ref={contentRef}>
+        <div className="grid grid-cols-1 gap-3 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+          <CardOne />
+          <CardTwo />
+          <CardThree />
+          <CardFour />
+        </div>
 
-      <div className="mt-3 md:gap-6 2xl:gap-7.5">
-        {/* <ChartFour /> */}
-        <BarChart />
-      </div>
+        <div className="mt-3 md:gap-6 2xl:gap-7.5">
+          {/* <ChartFour /> */}
+          <BarChart />
+        </div>
 
-      <div className="mt-3 md:gap-6 2xl:gap-7.5">
-        <BarChartII />
-      </div>
+        <div className="mt-3 md:gap-6 2xl:gap-7.5">
+          <BarChartII />
+        </div>
 
-      <div className="mt-3 md:gap-6 2xl:gap-7.5">
-        <AreaChart />
-      </div>
+        <div className="mt-3 md:gap-6 2xl:gap-7.5">
+          <AreaChart />
+        </div>
 
-      <div className="mt-3 grid grid-cols-10 gap-3 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-        <PieChart />
-        <DoughnutChart />
-        <RadarChart />
-        <GroupedBar />
-      </div>
+        <div className="mt-3 grid grid-cols-10 gap-3 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+          <PieChart />
+          <DoughnutChart />
+          <RadarChart />
+          <GroupedBar />
+        </div>
 
-      <div className="mt-3 md:gap-6 2xl:gap-7.5">
-        <ScatterChart />
-      </div>
+        <div className="mt-3 md:gap-6 2xl:gap-7.5">
+          <ScatterChart />
+        </div>
 
-      <div className="mt-3 md:gap-6 2xl:gap-7.5">
-        <BubbleChart />
-      </div>
+        <div className="mt-3 md:gap-6 2xl:gap-7.5">
+          <BubbleChart />
+        </div>
 
-      <div className="text-center p-10 border border-solid mt-3 bg-white rounded-xl w-full h-full">
-        <Map chosenKey="world"/>
+        <div className="text-center p-10 border border-solid mt-3 bg-white rounded-xl w-full h-full">
+          <Map chosenKey="world" />
+        </div>
+      </div>
+      <div className="text-center mt-3">
+        <button
+          className="bg-orange-400 hover:bg-orange-500 p-2 rounded-lg"
+          onClick={exportToPDF}
+        >
+          Exportar a PDF
+        </button>
       </div>
     </>
   );
--- IA_Marketing/src/pages/Profile.jsx
+++ IA_Marketing/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
-import Breadcrumb from '../components/Breadcrumb';
-import CoverOne from '../images/cover/cover-01.png';
-import userSix from '../images/user/user.png';
+import Breadcrumb from "../components/Breadcrumb";
+import CoverOne from "../images/cover/cover-01.png";
+import userSix from "../images/user/user.png";
 
 const Profile = () => {
   return (
@@ -123,9 +123,6 @@ const Profile = () => {
             </div>
 
             <div className="mt-6.5">
-              <h4 className="mb-3.5 font-medium text-black dark:text-white">
-                Redes sociales
-              </h4>
               <div className="flex items-center justify-center gap-3.5">
                 <a
                   href="#"
@@ -138,7 +135,6 @@ const Profile = () => {
                     height="22"
                     viewBox="0 0 22 22"
                     fill="none"
-
                   >
                     <g clipPath="url(#clip0_30_966)">
                       <path
@@ -164,7 +160,6 @@ const Profile = () => {
                     height="22"
                     viewBox="0 0 23 22"
                     fill="none"
-
                   >
                     <g clipPath="url(#clip0_30_970)">
                       <path
@@ -195,7 +190,6 @@ const Profile = () => {
                     height="22"
                     viewBox="0 0 23 22"
                     fill="none"
-
                   >
                     <g clipPath="url(#clip0_30_974)">
                       <path
@@ -226,7 +220,6 @@ const Profile = () => {
                     height="22"
                     viewBox="0 0 22 22"
                     fill="none"
-
                   >
                     <g clipPath="url(#clip0_30_978)">
                       <path
@@ -252,7 +245,6 @@ const Profile = () => {
                     height="22"
                     viewBox="0 0 23 22"
                     fill="none"
-
                   >
                     <g clipPath="url(#clip0_30_982)">
                       <path
@@ -275,6 +267,183 @@ const Profile = () => {
               </div>
             </div>
           </div>
+        </div>
+      </div>
+
+      <div className="mt-3 rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+        <div className="border-b border-stroke py-4 px-7 dark:border-strokedark">
+          <h3 className="font-medium text-black dark:text-white">
+            Actualizar Informacion Personal
+          </h3>
+        </div>
+        <div className="p-7">
+          <form action="#">
+            <div className="mb-5.5 flex flex-col gap-5.5 sm:flex-row">
+              <div className="w-full sm:w-1/2">
+                <label
+                  className="mb-3 block text-sm font-medium text-black dark:text-white"
+                  htmlFor="fullName"
+                >
+                  Nombre
+                </label>
+                <div className="relative">
+                  <span className="absolute left-4.5 top-4">
+                    <svg
+                      className="fill-current"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 20 20"
+                      fill="none"
+                    >
+                      <g opacity="0.8">
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M3.72039 12.887C4.50179 12.1056 5.5616 11.6666 6.66667 11.6666H13.3333C14.4384 11.6666 15.4982 12.1056 16.2796 12.887C17.061 13.6684 17.5 14.7282 17.5 15.8333V17.5C17.5 17.9602 17.1269 18.3333 16.6667 18.3333C16.2064 18.3333 15.8333 17.9602 15.8333 17.5V15.8333C15.8333 15.1703 15.5699 14.5344 15.1011 14.0655C14.6323 13.5967 13.9964 13.3333 13.3333 13.3333H6.66667C6.00363 13.3333 5.36774 13.5967 4.8989 14.0655C4.43006 14.5344 4.16667 15.1703 4.16667 15.8333V17.5C4.16667 17.9602 3.79357 18.3333 3.33333 18.3333C2.8731 18.3333 2.5 17.9602 2.5 17.5V15.8333C2.5 14.7282 2.93899 13.6684 3.72039 12.887Z"
+                          fill=""
+                        />
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M9.99967 3.33329C8.61896 3.33329 7.49967 4.45258 7.49967 5.83329C7.49967 7.214 8.61896 8.33329 9.99967 8.33329C11.3804 8.33329 12.4997 7.214 12.4997 5.83329C12.4997 4.45258 11.3804 3.33329 9.99967 3.33329ZM5.83301 5.83329C5.83301 3.53211 7.69849 1.66663 9.99967 1.66663C12.3009 1.66663 14.1663 3.53211 14.1663 5.83329C14.1663 8.13448 12.3009 9.99996 9.99967 9.99996C7.69849 9.99996 5.83301 8.13448 5.83301 5.83329Z"
+                          fill=""
+                        />
+                      </g>
+                    </svg>
+                  </span>
+                  <input
+                    className="w-full rounded border border-stroke bg-gray py-3 pl-11.5 pr-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+                    type="text"
+                    name="fullName"
+                    id="fullName"
+                    placeholder="Nombre de Usuario"
+                  />
+                </div>
+              </div>
+              <div className="w-full sm:w-1/2">
+                <label
+                  className="mb-3 block text-sm font-medium text-black dark:text-white"
+                  htmlFor="fullName"
+                >
+                  Apellido
+                </label>
+                <div className="relative">
+                  <span className="absolute left-4.5 top-4">
+                    <svg
+                      className="fill-current"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 20 20"
+                      fill="none"
+                    >
+                      <g opacity="0.8">
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M3.72039 12.887C4.50179 12.1056 5.5616 11.6666 6.66667 11.6666H13.3333C14.4384 11.6666 15.4982 12.1056 16.2796 12.887C17.061 13.6684 17.5 14.7282 17.5 15.8333V17.5C17.5 17.9602 17.1269 18.3333 16.6667 18.3333C16.2064 18.3333 15.8333 17.9602 15.8333 17.5V15.8333C15.8333 15.1703 15.5699 14.5344 15.1011 14.0655C14.6323 13.5967 13.9964 13.3333 13.3333 13.3333H6.66667C6.00363 13.3333 5.36774 13.5967 4.8989 14.0655C4.43006 14.5344 4.16667 15.1703 4.16667 15.8333V17.5C4.16667 17.9602 3.79357 18.3333 3.33333 18.3333C2.8731 18.3333 2.5 17.9602 2.5 17.5V15.8333C2.5 14.7282 2.93899 13.6684 3.72039 12.887Z"
+                          fill=""
+                        />
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M9.99967 3.33329C8.61896 3.33329 7.49967 4.45258 7.49967 5.83329C7.49967 7.214 8.61896 8.33329 9.99967 8.33329C11.3804 8.33329 12.4997 7.214 12.4997 5.83329C12.4997 4.45258 11.3804 3.33329 9.99967 3.33329ZM5.83301 5.83329C5.83301 3.53211 7.69849 1.66663 9.99967 1.66663C12.3009 1.66663 14.1663 3.53211 14.1663 5.83329C14.1663 8.13448 12.3009 9.99996 9.99967 9.99996C7.69849 9.99996 5.83301 8.13448 5.83301 5.83329Z"
+                          fill=""
+                        />
+                      </g>
+                    </svg>
+                  </span>
+                  <input
+                    className="w-full rounded border border-stroke bg-gray py-3 pl-11.5 pr-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+                    type="text"
+                    name="fullName"
+                    id="fullName"
+                    placeholder="Apellido de Usuario"
+                  />
+                </div>
+              </div>
+
+              
+            </div>
+
+            <div className="mb-5.5">
+              <label
+                className="mb-3 block text-sm font-medium text-black dark:text-white"
+                htmlFor="Username"
+              >
+                Empresa
+              </label>
+              <input
+                className="w-full rounded border border-stroke bg-gray py-3 px-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+                type="text"
+                name="Username"
+                id="Username"
+                placeholder="Empresa"
+              />
+            </div>
+
+            <div className="mb-5.5">
+              <label
+                className="mb-3 block text-sm font-medium text-black dark:text-white"
+                htmlFor="Username"
+              >
+                Sobre mi
+              </label>
+              <div className="relative">
+                <span className="absolute left-4.5 top-4">
+                  <svg
+                    className="fill-current"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                  >
+                    <g opacity="0.8" clipPath="url(#clip0_88_10224)">
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M1.56524 3.23223C2.03408 2.76339 2.66997 2.5 3.33301 2.5H9.16634C9.62658 2.5 9.99967 2.8731 9.99967 3.33333C9.99967 3.79357 9.62658 4.16667 9.16634 4.16667H3.33301C3.11199 4.16667 2.90003 4.25446 2.74375 4.41074C2.58747 4.56702 2.49967 4.77899 2.49967 5V16.6667C2.49967 16.8877 2.58747 17.0996 2.74375 17.2559C2.90003 17.4122 3.11199 17.5 3.33301 17.5H14.9997C15.2207 17.5 15.4326 17.4122 15.5889 17.2559C15.7452 17.0996 15.833 16.8877 15.833 16.6667V10.8333C15.833 10.3731 16.2061 10 16.6663 10C17.1266 10 17.4997 10.3731 17.4997 10.8333V16.6667C17.4997 17.3297 17.2363 17.9656 16.7674 18.4344C16.2986 18.9033 15.6627 19.1667 14.9997 19.1667H3.33301C2.66997 19.1667 2.03408 18.9033 1.56524 18.4344C1.0964 17.9656 0.833008 17.3297 0.833008 16.6667V5C0.833008 4.33696 1.0964 3.70107 1.56524 3.23223Z"
+                        fill=""
+                      />
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M16.6664 2.39884C16.4185 2.39884 16.1809 2.49729 16.0056 2.67253L8.25216 10.426L7.81167 12.188L9.57365 11.7475L17.3271 3.99402C17.5023 3.81878 17.6008 3.5811 17.6008 3.33328C17.6008 3.08545 17.5023 2.84777 17.3271 2.67253C17.1519 2.49729 16.9142 2.39884 16.6664 2.39884ZM14.8271 1.49402C15.3149 1.00622 15.9765 0.732178 16.6664 0.732178C17.3562 0.732178 18.0178 1.00622 18.5056 1.49402C18.9934 1.98182 19.2675 2.64342 19.2675 3.33328C19.2675 4.02313 18.9934 4.68473 18.5056 5.17253L10.5889 13.0892C10.4821 13.196 10.3483 13.2718 10.2018 13.3084L6.86847 14.1417C6.58449 14.2127 6.28409 14.1295 6.0771 13.9225C5.87012 13.7156 5.78691 13.4151 5.85791 13.1312L6.69124 9.79783C6.72787 9.65131 6.80364 9.51749 6.91044 9.41069L14.8271 1.49402Z"
+                        fill=""
+                      />
+                    </g>
+                    <defs>
+                      <clipPath id="clip0_88_10224">
+                        <rect width="20" height="20" fill="white" />
+                      </clipPath>96
+                    </defs>
+                  </svg>
+                </span>
+
+                <textarea
+                  className="w-full rounded border border-stroke bg-gray py-3 pl-11.5 pr-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+                  name="bio"
+                  id="bio"
+                  rows={6}
+                  placeholder="Write your bio here"
+                ></textarea>
+              </div>
+            </div>
+
+            <div className="flex justify-end gap-4.5">
+              <button
+                className="flex justify-center rounded border border-stroke py-2 px-6 font-medium text-black hover:shadow-1 dark:border-strokedark dark:text-white hover:bg-slate-50"
+                type="submit"
+              >
+                Cancelar
+              </button>
+              <button
+                className="flex justify-center rounded bg-orange-400 hover:bg-orange-500 py-2 px-6 font-medium text-gray hover:shadow-1"
+                type="submit"
+              >
+                Guardar
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </>
